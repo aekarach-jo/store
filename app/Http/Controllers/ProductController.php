ea0c481a5d85c75681fe5a1e36757204e0b0c2d0
Suphachai Prathumwan <Suphachai.P@WynnSoft-Solution.com>
2022-05-10 16:07:06 +0700

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -47,24 +47,22 @@ class ProductController extends Controller
 
     public function UpdateProduct(Request $request)
     {
+        
         // $request->validate([
         //     'product_name' => 'required',
         //     'category' => 'required',
         //     'unit' => 'required',
         // ]);
-
+        // dd($request->image[0]);
         $image = $this->uploadImage($request->image[0]);
+        // dd($image);
         $product = Product::where('id', $request->id)->update([
             'product_name' => $request->product_name,
             'category' => $request->category,
             'unit' => $request->unit,
-            'image' => $image,
-            'store_id' => $request->store_id
+            'image' =>  $image
+            // 'image' => ($request->image == null) ? "" : $request->image,
         ]);
-    }
-
-    public function DestroyProduct($id){
-        Product::where('id', $id)->delete();
     }
 
 
